131e583f4746530409be468505c4046b2ea1e567
PerhapsSPY <perhapsspy@gmail.com>
2017-01-17 16:15:06 +0900

    스크립트 경로 수정 / 도커 캐시 다시 넣어봄

--- etc/deploy/docker_push.sh
+++ etc/deploy/docker_push.sh
@@ -4,12 +4,19 @@ if  [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]
   eval $(aws ecr get-login --region ap-northeast-1)
   # DOCKER
   docker --version
+  # - cache load
+  if [[ -e docker/image.tar ]]; then
+   docker load -i ${HOME}/docker/image.tar
+  fi
   # - build and push
   docker build -t ${IMAGE_NAME} .
   echo "Pushing ${IMAGE_NAME}:latest"
   docker tag ${IMAGE_NAME}:latest "${REMOTE_IMAGE_URL}:latest"
   docker push "${REMOTE_IMAGE_URL}:latest"
   echo "Pushed ${IMAGE_NAME}:latest"
+  # - cache save
+  mkdir -p ~/docker;
+  docker save ${IMAGE_NAME}:latest > ${HOME}/docker/image.tar
 else
   echo "Skipping deploy because branch is not 'master'"
 fi
--- etc/deploy/ecs_update_service.sh
+++ etc/deploy/ecs_update_service.sh
@@ -2,7 +2,7 @@
 if  [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]]; then
   # Deploy only if we're testing the master branch
   echo "Deploying ${TRAVIS_BRANCH} on ${TASK_DEFINITION}"
-  ./ecs-deploy.sh -c ${TASK_DEFINITION} -n ${SERVICE_NAME} -i ${REMOTE_IMAGE_URL}:latest
+  etc/depoly/./ecs-deploy.sh -c ${TASK_DEFINITION} -n ${SERVICE_NAME} -i ${REMOTE_IMAGE_URL}:latest
 else
   echo "Skipping deploy because it's not an allowed branch"
 fi

74cf6e320c68a67a4c00144a474f3816d82049f3
PerhapsSPY <perhapsspy@gmail.com>
2017-01-17 16:43:52 +0900

    불필요하게 나눴던 스크립트 파일 지우고 .travis.yml에 합침

--- etc/deploy/ecs_update_service.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-if  [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]]; then
-  # Deploy only if we're testing the master branch
-  echo "Deploying ${TRAVIS_BRANCH} on ${TASK_DEFINITION}"
-  etc/depoly/./ecs-deploy.sh -c ${TASK_DEFINITION} -n ${SERVICE_NAME} -i ${REMOTE_IMAGE_URL}:latest
-else
-  echo "Skipping deploy because it's not an allowed branch"
-fi

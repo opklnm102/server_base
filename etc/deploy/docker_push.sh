758f3382cd009e7058eb136095ed8592bef78a0c
PerhapsSPY <perhapsspy@gmail.com>
2017-01-17 13:02:55 +0900

    빌드 시간 단축을 위해 캐시를 적용 중

--- etc/deploy/docker_push.sh
+++ etc/deploy/docker_push.sh
@@ -1,12 +1,21 @@
 #! /bin/bash
-# aws login
-pip install awscli
+# AWS LOGIN
 eval $(aws ecr get-login --region ap-northeast-1)
 
-# docker build and push
+# DOCKER
 docker --version
+# - cache load
+if [ -f ${DOCKER_CACHE_FILE} ]; then
+    gunzip -c ${DOCKER_CACHE_FILE} | docker load
+fi
+# - build and push
 docker build -t $IMAGE_NAME .
 echo "Pushing $IMAGE_NAME:latest"
 docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
 docker push "$REMOTE_IMAGE_URL:latest"
 echo "Pushed $IMAGE_NAME:latest"
+# - cache save
+if [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]];then
+    mkdir -p $(dirname ${DOCKER_CACHE_FILE})
+    docker save $(docker history -q ${REMOTE_IMAGE_URL}:latest | grep -v '<missing>') | gzip > ${DOCKER_CACHE_FILE}
+fi

f5bee56c74d9020107429dfa5fc2307f3b38f3a3
PerhapsSPY <perhapsspy@gmail.com>
2017-01-17 15:53:31 +0900

    ecs에 배포하는 스크립트 넣고 환경변수 정리함

--- etc/deploy/docker_push.sh
+++ etc/deploy/docker_push.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
-# AWS LOGIN
-eval $(aws ecr get-login --region ap-northeast-1)
-
-# DOCKER
-docker --version
-# - build and push
-docker build -t $IMAGE_NAME .
-echo "Pushing $IMAGE_NAME:latest"
-docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
-docker push "$REMOTE_IMAGE_URL:latest"
-echo "Pushed $IMAGE_NAME:latest"
+if  [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]]; then
+  # AWS LOGIN
+  eval $(aws ecr get-login --region ap-northeast-1)
+  # DOCKER
+  docker --version
+  # - build and push
+  docker build -t ${IMAGE_NAME} .
+  echo "Pushing ${IMAGE_NAME}:latest"
+  docker tag ${IMAGE_NAME}:latest "${REMOTE_IMAGE_URL}:latest"
+  docker push "${REMOTE_IMAGE_URL}:latest"
+  echo "Pushed ${IMAGE_NAME}:latest"
+else
+  echo "Skipping deploy because branch is not 'master'"
+fi

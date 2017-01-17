3ff2eed92ae57a3f2fe60d8f9f91d7e1fa1c243c
PerhapsSPY <perhapsspy@gmail.com>
2017-01-17 11:43:11 +0900

    travis에 docker 빌드, 푸시 넣어봄

---
+++ etc/deploy/docker_push.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# aws login
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+eval $(aws ecr get-login --region ap-northeast-1)
+
+# docker build and push
+docker --version
+docker build -t $IMAGE_NAME .
+echo "Pushing $IMAGE_NAME:latest"
+docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
+docker push "$REMOTE_IMAGE_URL:latest"
+echo "Pushed $IMAGE_NAME:latest"

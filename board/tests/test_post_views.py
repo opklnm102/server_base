6744e882e9fa2d89651e40fcf5e5bfd11e049f88
PerhapsSPY <perhapsspy@gmail.com>
2017-01-19 14:38:21 +0900

    테스트용 DB가 변경될때 문제 없게 id값 직접 넣은걸 참조로 바꿈

--- board/tests/test_post_views.py
+++ board/tests/test_post_views.py
@@ -19,11 +19,11 @@ class TestPostViewCase(TestCase):
         self.user = mommy_make_user()
         self.user_other = mommy_make_user()
         self.user_staff = mommy_make_user(is_staff=True)
-        category_news = mm(Category, name='뉴스', slug='news')
-        category_technology = mm(Category, parent=category_news, name='기술', slug='technology')
+        self.category_news = mm(Category, name='뉴스', slug='news')
+        self.category_technology = mm(Category, parent=self.category_news, name='기술', slug='technology')
         self.posts = mm(Post, user=self.user, _quantity=3)
         for post in self.posts:
-            post.categories.add(category_technology)
+            post.categories.add(self.category_technology)
 
     def test_list_view(self):
         response = self.get_check_200('board:post-list')
@@ -34,12 +34,14 @@ class TestPostViewCase(TestCase):
         with self.login(username=self.user):
             self.post(
                 'board:post-list',
-                data={'title': 'hi', 'content': 'a', 'categories': [1, 2]}
+                data={'title': 'hi', 'content': 'a',
+                      'categories': [self.category_news.id, self.category_technology.id]}
             )
             self.response_201()
             self.put(
                 'board:post-detail', pk=post.id,
-                data=json.dumps({'title': 'hi', 'content': 'b', 'categories': [2, ]}),
+                data=json.dumps({'title': 'hi', 'content': 'b',
+                                 'categories': [self.category_technology.id, ]}),
                 extra={'content_type': 'application/json'}
             )
             self.response_200()
@@ -55,12 +57,14 @@ class TestPostViewCase(TestCase):
         with self.login(username=self.user_staff):
             self.post(
                 'board:post-list',
-                data={'title': 'notice', 'content': "I'm staff", 'categories': [1, 2]}
+                data={'title': 'notice', 'content': "I'm staff",
+                      'categories': [self.category_news.id, self.category_technology.id]}
             )
             self.response_201()
             response = self.put(
                 'board:post-detail', pk=post.id,
-                data=json.dumps({'title': 'hi', 'content': 'edited by staff', 'categories': [1, ]}),
+                data=json.dumps({'title': 'hi', 'content': 'edited by staff',
+                                 'categories': [self.category_news.id, ]}),
                 extra={'content_type': 'application/json'}
             )
             assert post.user.id == response.json().get('user')

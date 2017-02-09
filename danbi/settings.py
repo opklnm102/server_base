676ae447e63e95c35e81ca9346e5d4ce008c1ca5
PerhapsSPY <perhapsspy@gmail.com>
2017-02-09 15:53:54 +0900

    django_extensions 앱에 추가

--- danbi/settings.py
+++ danbi/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 3rd party
+    'django_extensions',
     'rest_framework',
     'rest_framework.authtoken',
     'django_summernote',

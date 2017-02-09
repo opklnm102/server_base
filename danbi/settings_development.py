29f7e3603908f04a1dcf8999e7cdaf7f45662766
PerhapsSPY <perhapsspy@gmail.com>
2017-02-09 12:49:54 +0900

    django debug toolbar 설정

--- danbi/settings_development.py
+++ danbi/settings_development.py
@@ -2,3 +2,7 @@ try:
     from .settings import *
 except ImportError:
     pass
+
+INTERNAL_IPS = ['127.0.0.1']
+INSTALLED_APPS.append('debug_toolbar')
+MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')

a9a36ae1da8f47ab59c544a257b437705bf362b9
PerhapsSPY <perhapsspy@gmail.com>
2017-02-23 14:55:33 +0900

    #4 덤프 테스크 완료시 row 수를 로깅하도록 함

--- board/tasks.py
+++ board/tasks.py
@@ -1,9 +1,14 @@
 from __future__ import absolute_import
-from danbi.celery import app
+
+import logging
 
 from board.management.commands.dump_board_post import dump_post
+from danbi.celery import app
+
+logger = logging.getLogger(__name__)
 
 
 @app.task
 def dump_board_post_task():
-    dump_post()
+    rows = dump_post()
+    logger.info('{} posts'.format(rows))
